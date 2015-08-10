547a17c9b520d12ee66a562ad025492f550f41ce
Chad Dotson <chad@cdotson.com>
2015-08-09 23:10:33 -0500

    Add tests project.  And twitter_tests.

---
+++ tests/twitter_tests.py
@@ -0,0 +1,14 @@
+
+from unittest import TestCase
+
+
+class TwitterTests(TestCase):
+    def test_can_get_twitter_api(self):
+        self.assertTrue(False)
+
+    def test_can_create_twitter_dm_object(self):
+        self.assertTrue(False)
+
+    def test_can_encode_twitter_dm_text_message(self):
+        self.assertTrue(False)
+
